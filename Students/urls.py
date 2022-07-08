8ba03ef0663caf70e559f9a54efd94772a29ea0d
Shadrack Opoku <shaddyopoku1@gmail.com>
2022-07-08 17:32:31 +0000

    Added create student url

--- Students/urls.py
+++ Students/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Student_minimal_view, Create_student_profile_serializer, Detail_update_student_profile_view
+from .views import Student_minimal_view, Create_student_profile_view, Detail_update_student_profile_view
 from .prefect_views import Prefect_view, Prefect_detail_view
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     #student views...
     path('', Student_minimal_view.as_view(), name='Student_min_view'),
+    path('create_student/', Create_student_profile_view.as_view(), name="create student profile"),
     #Detail student view
     path('<int:pk>/',
          Detail_update_student_profile_view.as_view(),
